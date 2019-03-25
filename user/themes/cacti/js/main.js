0a5ef87891666e49cb443bf05aa8e260513c022d
Korobeynik <korobeynik77@gmail.com>
2019-03-25 17:52:58 +0200

    Update theme 25.03

--- user/themes/cacti/js/main.js
+++ user/themes/cacti/js/main.js
@@ -2,4 +2,23 @@ $(document).ready(function () {
 	$('.btn-call-back').click(function (e) {
 		e.preventDefault();
 	})
+	
+	$("#form, #form2, #form3").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			// alert("Спасибо! Мы скоро свяжемся с Вами.");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+				$('.close-modal').trigger('click');
+			}, 1000);
+		});
+		return false;
+	});
+	
+	
 });
